80afd8c7659ba99eebc63f60d3c5fde53d0ba1f3
M Naufal Ammar R <mnaufalammarr.15@gmail.com>
2023-10-10 08:12:57 +0700

    Tugas Hari-6 Metadata

--- sanber-nextjs/pages/index.js
+++ sanber-nextjs/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "@/layout";
 export default function Home() {
  return (
   <div>
-<Layout>
+<Layout metaTitle="Home" metaDescription="Ini adalah halaman Home">
     <h1>Home</h1>
     <p>Ini adalah halaman Home</p>
 </Layout>
--- sanber-nextjs/pages/profile.js
+++ sanber-nextjs/pages/profile.js
@@ -3,7 +3,7 @@ import Layout from "@/layout";
 export default function Profile() {
     return (
     <div>
-    <Layout>
+    <Layout metaTitle="Profile" metaDescription="Semua informasi ini adalah seputar User">
         <h1>Profile</h1>
         <p>Ini adalah halaman Profile</p>
     </Layout>
