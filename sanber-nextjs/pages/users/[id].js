4707dab6c05ef4cbce168573e2d3410a1c24c7a3
M Naufal Ammar R <mnaufalammarr.15@gmail.com>
2023-10-11 21:18:16 +0700

    Tugas Hari-8 Dynamic Routing

---
+++ sanber-nextjs/pages/users/[id].js
@@ -0,0 +1,18 @@
+import Layout from "@/layout";
+import { useRouter } from "next/router";
+
+export default function UserByName() {
+    const router = useRouter();
+    const { id } = router.query;
+    return (
+    <div>
+      <Layout
+        metaTitle="User By Name"
+        metaDescription="Semua informasi ini adalah seputar User By Name"
+      >
+        <h1>User By Name</h1>
+        <p>Hi! {id}</p>
+      </Layout>
+    </div>
+  );
+}
