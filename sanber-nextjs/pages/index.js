950465b794f978536b6be294cfc74e5a51a39801
M Naufal Ammar R <mnaufalammarr.15@gmail.com>
2023-10-13 08:27:29 +0700

    Tugas Hari-9 API Routing

--- sanber-nextjs/pages/index.js
+++ sanber-nextjs/pages/index.js
@@ -1,13 +1,19 @@
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Home() {
- return (
-  <div>
-<Layout metaTitle="Home" metaDescription="Ini adalah halaman Home">
-    <h1>Home</h1>
-    <p>Ini adalah halaman Home</p>
-</Layout>
-
-  </div>
- );
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("Response => ", res))
+      .catch((err) => console.log("Error => ", err));
+  }, []);
+  return (
+    <div>
+      <Layout metaTitle="Home" metaDescription="Ini adalah halaman Home">
+        <h1>Home</h1>
+        <p>Ini adalah halaman Home</p>
+      </Layout>
+    </div>
+  );
 }
